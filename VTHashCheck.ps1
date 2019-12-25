52d904a0048e6050e02caaeb418bedad2c2ea687
ESDaman <30635958+ESDaman@users.noreply.github.com>
2019-12-25 10:10:18 -0600

    Add files via upload

---
+++ VTHashCheck.ps1
@@ -0,0 +1,10 @@
+﻿#Replace "<APIKEY>" with your API key...
+Get-Content .\hashes.txt | foreach-object {
+$webcontent = curl -uri "https://www.virustotal.com/vtapi/v2/file/report?apikey=<APIKEY>&resource=$_"
+$converted = $webcontent.content | ConvertFrom-json
+if ($converted.positives -gt 0)
+{
+$_ >> .\results.txt
+$converted.positives >> .\results.txt
+}
+}
